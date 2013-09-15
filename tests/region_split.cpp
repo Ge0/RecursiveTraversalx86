1c5325ac103dc5f9d849cf1fc9f2c5ee2883e063
Geoffrey R. <geoffrey.royer@gmail.com>
2013-09-15 19:33:54 +0200

    furthering region split test

--- tests/region_split.cpp
+++ tests/region_split.cpp
@@ -1,6 +1,24 @@
 #include <cstdlib>
 #include <set>
+#include <stack>
+#include <libdasm.h>
+#include <iostream>
+
+#include "BinaryBlock.hpp"
 #include "BinaryRegion.hpp"
+
+bool is_address_within_blocks(const int64_t& address, std::set<BinaryBlock*>& binaryBlocks) {
+	std::set<BinaryBlock*>::const_iterator it = binaryBlocks.begin();
+	bool match = false;
+	
+	while(!match && it != binaryBlocks.end()) {
+		if(address >= (*it)->address() && address <= (*it)->address() + (*it)->length()) {
+			match = !match;
+		}
+		++it;
+	}
+}
+void analyze_binary_region(const BinaryRegion& binaryRegion, std::set<BinaryBlock*>& binaryBlocks);
 
 int main(int argc, char** argv) {
 	BinaryRegion binaryRegion(
@@ -11,5 +29,15 @@ int main(int argc, char** argv) {
 	);
 	
 	return EXIT_SUCCESS;
+	
+}
+
+void analyze_binary_region(const BinaryRegion& binaryRegion, std::set<BinaryBlock*>& binaryBlocks) {
+	int64_t current_memory_address = binaryRegion.baseAddress() + binaryRegion.entryPointOffset();
+	
+	// The current_memory_address should not be into any pre-existing binary block
+	if(is_address_within_blocks(current_memory_address, binaryBlocks)) {
+		return;
+	}
 	
 }
