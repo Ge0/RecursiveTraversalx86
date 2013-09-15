4b7569cf5956a5eed92f2a8b9c5a03a2950dc5c0
Geoffrey R. <geoffrey.royer@gmail.com>
2013-09-15 20:45:16 +0200

    header forgotten

--- include/RecursiveTraversalInstructionProcessor.hpp
+++ include/RecursiveTraversalInstructionProcessor.hpp
@@ -2,6 +2,7 @@
 #define _RECURSIVE_TRAVERSAL_PROCESSOR_INSTRUCTION_HPP_
 
 #include "AbstractInstructionProcessor.hpp"
+#include <stack>
 
 class RecursiveTraversalInstructionProcessor : public AbstractInstructionProcessor {
 public:
@@ -9,6 +10,9 @@ public:
 	virtual void processReferencingInstruction(const ReferencingInstruction&);
 	virtual void processFlowInstruction(const FlowInstruction&);
 	virtual void processHijackFlowInstruction(const HijackFlowInstruction&);
+	
+private:
+	std::stack<int64_t> m_addressesToDisassemble;
 };
 
 #endif /* _RECURSIVE_TRAVERSAL_PROCESSOR_INSTRUCTION_HPP_ */
